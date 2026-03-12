17e120751c7a3891e80c9ffb33393eb2934d17e0
Den <nvardanyan716@gmail.com>
2026-03-12 04:54:51 +0400

    Исправления PEP8

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+
 def about(request):
     template = 'pages/about.html'
     return render(request, template)
 
+
 def rules(request):
     template = 'pages/rules.html'
     return render(request, template)
+    
